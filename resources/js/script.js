fe0875243cfef528ff6de22c379c2675c48f8ce3
mmaholy1991 <mmaholy1991@yahoo.fr>
2019-11-06 15:46:17 +0100

    Tutorial Jquery
    exemple simple

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,10 @@
 /***TUTORIAL JQUERY RAPIDE**/
-$(document).ready(function(){
+jQuery(document).ready(function(){
    /*fonction à appeler quand la page est chargé $(document) est notre page*/ 
+    $('h1').click(function(){
+    /*$(this) ici est l'élément selectionné juste avant*/
+    /*on peut appeler un css à partir de jquery et changer le css à la volée*/
+    $(this).css('background-color','#FF0000')
+    })
+    
 });
