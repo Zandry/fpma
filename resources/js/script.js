9b7181d93e963253e1dd06b4faee886b5d8f8442
mmaholy1991 <mmaholy1991@yahoo.fr>
2019-11-27 16:05:12 +0100

    Nav Responsive - solution - mise en place bouton croix pour fermer le menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -112,6 +112,16 @@ $('.js--plan-box').waypoint(
     */
     $('.js--nav-icon').click(function(){
         var nav = $(".js--main-nav");
+        var icon = $(".js--nav-icon i");
         nav.slideToggle(200);
+        if (icon.hasClass("fa-bars"))
+            {
+                icon.addClass("fa-window-close");
+                icon.removeClass("fa-bars");
+            }
+        else{
+            icon.addClass("fa-bars");
+            icon.removeClass("fa-window-close");
+        }
     });
 });
